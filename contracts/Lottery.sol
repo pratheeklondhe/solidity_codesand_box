244be7a52a78f580822e0be61c1b181b8970fb82
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-02 11:16:18 +0530

    solidity Lottery contract added

---
+++ contracts/Lottery.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enroll() public payable{
+        require(msg.value > 0.01 ether);
+
+        players.push(msg.sender);
+    }
+
+    function randomNumberGenerator() private view returns(uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    function pickWinner() public onlyManager {
+        require(players.length > 0);
+
+        uint random = randomNumberGenerator() % players.length;
+        payable(players[random]).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    modifier onlyManager {
+        require(msg.sender == manager);
+        _;
+    }
+
+
+}
