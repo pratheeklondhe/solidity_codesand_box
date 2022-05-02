71f025d04392a658b0ac74298139403d1d4310a4
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-02 20:13:25 +0530

    wallet connection

---
+++ deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider({
+	mnemonic: {
+		phrase:
+			'fun device gentle try sauce risk effort sound net bone define chase'
+	},
+	providerOrUrl: 'https://goerli.infura.io/v3/0cf52fce48db492282b6d25ad739f828'
+});
+const web3 = new Web3(provider);
+
+async function deploy() {
+	try {
+		const accounts = await web3.eth.getAccounts();
+
+		const result = await new web3.eth.Contract(abi)
+			.deploy({ data: bytecode })
+			.send({ from: accounts[0], gas: '1000000' });
+
+		console.log(result);
+		console.log(result.options.address);
+	} catch (err) {
+		console.log(err);
+	}
+}
+deploy();
