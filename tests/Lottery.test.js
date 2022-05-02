27d112ae961425826afb744798cc2ca2316737c6
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-02 11:55:37 +0530

    deploy contract using ganache done

--- tests/Lottery.test.js
+++ tests/Lottery.test.js
@@ -5,4 +5,12 @@ const web3 = new Web3(ganache.provider());
 
 const { abi, bytecode } = require("../compile");
 
-console.log(bytecode);
+(async () => {
+  let accounts = await web3.eth.getAccounts();
+
+  let lottery = await new web3.eth.Contract(abi)
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  console.log(lottery);
+})();
