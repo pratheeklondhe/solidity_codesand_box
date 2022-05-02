9262b4fa13310df2dd7f1d17238c7b90582ada0b
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-02 13:42:34 +0530

    test: allows one account to enroll

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,7 +1,5 @@
 const assert = require("assert");
 const ganache = require("ganache");
-// let expect = require("chai").expect;
-// const { before, beforeEach } = require("mocha");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
@@ -21,5 +19,17 @@ beforeEach(async () => {
 describe("Lottery Contract Test Suite", () => {
   it("deploys contract", () => {
     assert.ok(lottery);
+  });
+
+  it("allows one account to enroll", async () => {
+    await lottery.methods.enroll().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+
+    const players = await lottery.methods.getPlayer().call({
+      from: accounts[0]
+    });
+    assert.eq(players.length);
   });
 });
