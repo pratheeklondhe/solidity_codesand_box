e3518c798e683e04b1e89682e674ee09bea3ccab
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-19 20:10:40 +0530

    latest compiled contracts

--- latest-scripts/latest-compile.js
+++ latest-scripts/latest-compile.js
@@ -28,8 +28,8 @@ const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
 ];
 
 for (let contract in output) {
-	console.log(Object.keys(output[contract].evm));
-	// fs.outputJsonSync(buildPath + '/' + contract + '.json', output[contract]);
+	// console.log(Object.keys(output[contract].evm));
+	fs.outputJsonSync(buildPath + '/' + contract + '.json', output[contract]);
 }
 
 console.log('Compiled and outputted to builds folder');
